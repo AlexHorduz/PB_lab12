35431012be9bfa3a814cf99dcfff53a3f088e8ba
AlexHorduz <74256406+AlexHorduz@users.noreply.github.com>
2021-02-12 10:21:20 +0200

    Update Films_DB.java
    Made DB non-static

--- Films_DB.java
+++ Films_DB.java
@@ -3,33 +3,40 @@ package lab12_PB;
 import java.util.HashSet;
 
 public class Films_DB {
-    private static HashSet<Film> films = new HashSet<Film>();
-    private static HashSet<Actor> actors = new HashSet<Actor>();
+	private HashSet<Film> films = new HashSet<Film>();
+    private HashSet<Actor> actors = new HashSet<Actor>();
 
-    private Films_DB() {
+    public Films_DB() {
+        this.films = new HashSet<Film>();
+        this.actors = new HashSet<Actor>();
+    }
+    
+    public Films_DB(HashSet<Film> films, HashSet<Actor> actors) {
+    	this.films = films;
+    	this.actors = actors;
     }
 
-    public static void addFilm(Film film) {
+    public void addFilm(Film film) {
         films.add(film);
     }
 
-    public static boolean removeFilm(Film film) {
+    public boolean removeFilm(Film film) {
         return films.remove(film);
     }
 
-    public static void addActor(Actor actor) {
+    public void addActor(Actor actor) {
         actors.add(actor);
     }
 
-    public static boolean removeActor(Actor actor) {
+    public boolean removeActor(Actor actor) {
         return actors.remove(actor);
     }
 
-    public static HashSet<Film> getFilms() {
+    public HashSet<Film> getFilms() {
         return (HashSet<Film>) films.clone();
     }
 
-    public static HashSet<Actor> getActors() {
+    public HashSet<Actor> getActors() {
         return (HashSet<Actor>) actors.clone();
     }
 } 
