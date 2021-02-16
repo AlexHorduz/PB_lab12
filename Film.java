9f4e0cdabe3eb88eecd1c29cadc493290c69a884
AlexHorduz <74256406+AlexHorduz@users.noreply.github.com>
2021-02-16 15:47:36 +0200

    Update Film.java
    another hashCode() update

--- Film.java
+++ Film.java
@@ -23,7 +23,11 @@ public class Film {
     @Override
     public int hashCode()
     {
-        return name.hashCode() + 17 * actors.hashCode();
+    	final int prime = 17;
+		int result = 1;
+		result = prime * result + name.hashCode();
+		result = prime * result + actors.hashCode();
+		return result;
     }
     
     @Override
